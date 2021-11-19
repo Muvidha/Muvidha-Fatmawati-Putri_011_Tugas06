81f055ca4a144f434e5dcffdb86141df7e96039d
Muvidha <90436781+Muvidha@users.noreply.github.com>
2021-11-19 20:23:22 +0700

    Add files via upload

---
+++ Muvidha FP_011_POINTER MAXS.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	int n,a,max;
+	int *ptr_max;
+	
+
+	cout<<"Nilai Maksimal dari N"<<endl;
+
+	cout<<"\n";
+	
+	cout<<"Jumlah Data Bilangan N : "; cin>>n;
+	cout<<"=========================="<<endl;
+	cout<<"\n";
+	
+	for (int i=1;i<=n;i++)
+	{
+		cout<<"Angka Data ke-"<<i<<" : "; cin>>a;
+		
+		
+		if (i==1){
+			max=a;
+		}
+		else if (max<a){
+			max=a;
+		}
+		else {}
+		
+		ptr_max=&max;
+		*ptr_max=max;
+	}
+	cout<<"=========================="<<endl;
+	cout<<"\n";
+	
+	cout<<"Nilai Maksimal Bilangan N : "<<*ptr_max<<endl;
+	
+	getch();
+}
